d3733349f0ea3c95e93831debfe2c4ded6c8e6ce
Nikhil_Ranga <87887727+nikhilranga4@users.noreply.github.com>
2024-12-19 16:15:54 +0530

    Update formRoutes.js

--- src/routes/formRoutes.js
+++ src/routes/formRoutes.js
@@ -1,5 +1,3 @@
-// formRoutes.js
-
 const express = require('express');
 const router = express.Router();
 const formController = require('../controllers/formController'); // Import form controller
@@ -12,5 +10,8 @@ router.post('/', formController.createForm);
 
 // Add questions to a form
 router.put('/add-questions', formController.addQuestionsToForm);
+
+// Delete a form by ID
+router.delete('/:formId', formController.deleteForm); // DELETE route for deleting form
 
 module.exports = router;
